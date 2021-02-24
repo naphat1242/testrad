5802dc5c1d54328b5929f929c38fdaf49a916563
Naphat Tiw <naphat1242@gmail.com>
2021-02-24 12:10:23 +0700

    PHPRad

---
+++ helpers/Menu.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+	public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'category', 
+			'label' => 'Category', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'post', 
+			'label' => 'Post', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'user', 
+			'label' => 'User', 
+			'icon' => ''
+		)
+	);
+
+	public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'category', 
+			'label' => 'Category', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'post', 
+			'label' => 'Post', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'user', 
+			'label' => 'User', 
+			'icon' => ''
+		)
+	);
+
+	
+	
+	public static $name = array();
+
+}
